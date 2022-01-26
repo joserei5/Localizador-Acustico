c725b83a5fcf42a42a512dfcc411f8c6b8228dd0
joserei5 <jreis@ua.pt>
2022-01-26 16:38:34 +0000

    fixed algorithm theoretical values

--- report_misc/chapter2/realvalues.m
+++ report_misc/chapter2/realvalues.m
@@ -1,42 +1,60 @@
 %% Initial cmds
 clear;clc;close all;
-addpath ../simLocUS
-addpath ../functions
-addpath ../soundfiles/generic
-addpath ../structures
+addpath ../../simLocUS
+addpath ../../functions
 
 %% Data
-delta_x = 0.1*2;
+delta_x = 29.2 * 1e-2;
 room.temp = 20;
+rcoord = [11 11 3]; % room coordinates
+hcoord = [0.5 rcoord(2)/2 1.7]; % head coordinates
 C = 20.05*sqrt(273.15+room.temp);
 
-% theoretical AOA
-r_theta = 0:0.5:180;
+% trajectory
+t_dist = 0.2;
+t_points = 500;
+t_offset = 0;
+tcoord=speaker_hcircle2D(rcoord,hcoord,t_dist,t_points,t_offset);
 
-% extremes value in seconds
-d_t_ext = [(cosd(0)*delta_x)/C (cosd(180)*delta_x)/C];
-d_t = linspace(d_t_ext(1), d_t_ext(2), length(r_theta));
+% theoretical AOA and
+% algorithm AOA
+SRC.traj.x = tcoord(:,1);
+SRC.traj.y = tcoord(:,2);
+ROOM.rec.x = hcoord(1);
+ROOM.rec.y = hcoord(2);
+ROOM.rec.dx = delta_x;
+ROOM.rec.azimuth = 0;
+AOA = getTrajAOA(SRC,ROOM);
 
-% calc AOA with algorithm
-arg = C*(d_t+0)./delta_x;
-arg(arg<-1) = -1;
-arg(arg>1) = 1;
-alg_theta = acosd(arg);
-error_theta = r_theta - alg_theta;
+% AOA error
+AOA.error = abs(AOA.theoretical - AOA.algorithm);
 
 %% Plot
+% room
+roomf=figure;
+rectangle('Position',[0 0 rcoord(1) rcoord(2)])
+% trajectory
+hold on;
+plot(tcoord(:,1), tcoord(:,2),'k');
+plot(tcoord(1,1), tcoord(1,2),'ko');
+hold off;
+% receiver
+hold on;
+plot(hcoord(1), hcoord(2), 'b+', 'LineWidth', 2);
+hold off;
+
 % results
 figure;
-plot(r_theta, r_theta, 'k');
+plot(AOA.theoretical, AOA.theoretical, 'k');
 hold on
-plot(r_theta, alg_theta, 'r');
+plot(AOA.theoretical, AOA.algorithm, 'r');
 hold off
-legend('Real AOA', 'Algorithm AOA')
-xlabel('source azimuth (º)')
-ylabel('output azimuth (º)')
+legend('Theoretical AOA', 'Algorithm AOA')
+xlabel('Theoretical AOA (º)')
+ylabel('Predicted AOA (º)')
 
 % errors
 figure;
-plot(r_theta, error_theta, 'b')
-xlabel('source azimuth (º)')
-ylabel('output azimuth error (º)')
+plot(AOA.theoretical, AOA.error, 'b')
+xlabel('Theoretical AOA (º)')
+ylabel('Predicted AOA error (º)')
