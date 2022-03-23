12547eca0915921f2ef946175c21d854a2cd3c41
joserei5 <jreis@ua.pt>
2022-03-23 01:19:05 +0000

    simpler progress information

--- functions/ideal_wav.m
+++ functions/ideal_wav.m
@@ -32,7 +32,7 @@ function ideal_wav(A, T, R, REC)
 
 %% Prepare main objects
 % floor (no walls)
-AW = addWall([], [ 0  0  0], [20  0 0], [20 20 0], [ 0 20 0], 0.5);
+AW = addWall([], [0  0  0], [20  0 0], [20 20 0], [0 20 0], 0.5);
 % receiver
 dir=pwd;
 if isunix
@@ -67,90 +67,71 @@ R.obj.P = R.P;
 %% Prepare trajectory (with interpolation)
 % generate waypoints
 waypoints = [T.data.x, T.data.y, T.data.z];
+
 % trajectory differences
 tt.DX = diff(T.data.x);
 tt.DX = [0; tt.DX];
 tt.DY = diff(T.data.y);
 tt.DY = [0; tt.DY];
+
 % trajectory distance estimation
 tt.D = sqrt(tt.DX.^2 + tt.DY.^2);
+
 % trajectory time
 tt.T = tt.D ./ T.data.v;
 tt.T = cumsum(tt.T);
+
 % define waypointTrajectory parameters
 SRATE = A.fs;
 FRAMES = SRATE;
+
 % waypointTrajectory setup
 trajectory = waypointTrajectory(waypoints,...
                                 'TimeOfArrival',tt.T,...
                                 'SampleRate', SRATE,...
                                 'SamplesPerFrame', FRAMES);
-% initialize dumping arrays
-tx = []; ty = []; tz = [];
+
 % get last dump index
 last_dump = round(round(tt.T(end))*(SRATE/FRAMES));
-% initialize time estimation variables
-tic;tm=0;tn=0;
 
-% initialize informative message box
-mb = msgbox("? minutes remaining","[ideal_wav.m: Trajectory Interpolation");
+% initialize dumping arrays
+a_size = tt.T(end)*last_dump;
+tx = zeros(a_size,1);
+ty = zeros(a_size,1);
+tz = zeros(a_size,1);
+
+% initialize time estimation variables
+tic;tm=0;
+
 % start the waypointTrajectory dumping cycle
+fprintf("[Rebuilding Trajectory] ? min\n");
 for i=1:last_dump
+   % indexing variables
+   i1 = 1 + (i-1)*SRATE;
+   i2 = i*SRATE;
+   i_range = i1:i2;
+   
+   % fill the dumping arrays
    dump = trajectory();
-   tx = [tx; dump(:,1)];
-   ty = [ty; dump(:,2)];
-   tz = [tz; dump(:,3)];
+   tx(i_range,1) = dump(:,1);
+   ty(i_range,1) = dump(:,2);
+   tz(i_range,1) = dump(:,3);
 
    % tn = minutes remaining
     ttt = toc;
     if ttt > tm + 1
         tm = ttt;
         tn = round((last_dump-i)*ttt/(60*i));
+        fprintf("[Rebuilding Trajectory] %d min\n",tn);
     end
-   txt = sprintf("%d minutes remaining",tn);
-   set(findobj(mb,'Tag','MessageBox'),'String',txt);
-   drawnow;
 end
-% close message box
-delete(mb);
+fprintf("[Rebuilding Trajectory] DONE.\n");
 
-%% Generate IR
-% initialize stereo channels
+% Trajectory length
 N = length(tx);
-y = zeros(2,N);
 
-% check if sound samples do not cover trajectory
-% (if they don't: repeat the samples until the length
-% of the array is the same length of the interpolated
-% trajectory array)
-if N > length(A.y)
-    A.y = repmat(A.y, ceil(N/length(A.y)), 1);
-end
-
-% speaker/source initial location
-S = addSpk([tx(1) ty(1) tz(1)]);
-
-% initialize time estimation variables
-% time variables
-tm = 0; tn=0;
-% start timer/clock
-clk = tic;
-
-% RECONSTRUCTION LOOP
-% initialize informative message box
-mb = msgbox("? minutes remaining","[ideal_wav.m: Spatial reconstruction");
+% fix NaN values
 for n=1:N
-    % skip on: out-of-boundaries coordinates
-    if tx(n) <= 0
-        continue
-    end
-    if ty(n) <= 0
-        continue
-    end
-    if tz(n) <= 0
-        continue
-    end
-    % fix NaN numbers
     if n>1
         if isnan(tx(n))
             tx(n) = tx(n-1);
@@ -171,8 +152,41 @@ for n=1:N
         if isnan(tz(n))
             tz(n) = T.data.z(1);
         end
-    end
+    end 
+end
+fprintf("[Fixing Trajectory] DONE.\n");
 
+%% Generate IR
+% initialize stereo channels
+y = zeros(2,N);
+
+% check if sound samples do not cover trajectory
+% (if they don't: repeat the samples until the length
+% of the array is the same length of the interpolated
+% trajectory array)
+if N > length(A.y)
+    A.y = repmat(A.y, ceil(N/length(A.y)), 1);
+end
+
+% speaker/source initial location
+S = addSpk([tx(1) ty(1) tz(1)]);
+
+% initialize time estimation variables
+% time variables
+tm = 0;
+% start timer/clock
+clk = tic;
+
+% prepare iteration array with values that respect
+% the room boundaries i.e. trajectory (x,y) values
+% that do not trespass the room boundaries.
+it_values = 1:N;
+it_mask = ~((tx<=0)|(ty<=0)|(tz<=0));
+it_values = it_values(it_mask);
+
+% RECONSTRUCTION LOOP  
+fprintf("[Stereo Reconstrunction] ? min\n");
+for n=it_values
     % feed data to speaker/moving object
     S.C(1) = tx(n);
     S.C(2) = ty(n);
@@ -198,14 +212,10 @@ for n=1:N
     if ttt > tm + 1
         tm = ttt;
         tn = round((N-n)*ttt/(60*n));
+        fprintf("[Stereo Reconstrunction] %d min\n",tn);
     end
-    
-   txt = sprintf("%d minutes remaining",tn);
-   set(findobj(mb,'Tag','MessageBox'),'String',txt); 
-   drawnow;
 end
-% close message box
-delete(mb);
+fprintf("[Stereo Reconstrunction] DONE.\n");
 
 % write stereo sample
 % (same name as the trajectory)
@@ -214,3 +224,4 @@ fname = datestr(now,'ddmmyyyy_HHMMSS');
 fpath = join([path,fname,'.wav']);
 audiowrite(fpath,y',A.fs)
 
+fprintf("%s.wav has been written to %s.\n\n",fname,path);
